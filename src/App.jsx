e365e69c66ca340e28b9d2b7f585b1fc2c667357
skarolk <skarolkiewicz@gmail.com>
2021-01-14 18:23:05 -0600

    test for googleoauth

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import NavMenu from "./Header/NavMenu/NavMenu";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+//@ts-ignore
+//tslint:disable-next-line
+import BoilerplateComponent from 'jw-react-npm-boilerplate';
+
 function App() {
   return (
     <>
@@ -14,6 +18,7 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <p>When you hate enough to send the very worst</p>
           <Button variant="outline-info">Buy Now</Button>
+          <BoilerplateComponent label="My React boilerplate component label: " onChange={val => console.log(val)} />
         </header>
       </div>
     </>
